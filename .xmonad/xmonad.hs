eb253328b05911986a99a046d408f1c835497497
Vladislav Zhavoronkov <v.zhvrnkv@gmail.com>
2020-11-16 23:49:38 +0600

    [arch] add xmobar to xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,28 +1,24 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
 import System.Environment
 import qualified Data.Map.Internal as DMI
 
-main = xmonad def
-     { terminal           = myTerminal
-     , layoutHook         = myDefaultLayout
-     , modMask            = myModMask
-     , borderWidth        = 0
---     , keys               = myKeys
-     }
+main = do
+  spawn "xmobar"
+  xmonad $ docks config
+    where config = def { terminal    = myTerminal
+                       , layoutHook  = myDefaultLayout
+                       , modMask     = myModMask
+                       , borderWidth = 0
+                       }
 
 myTerminal = "alacritty"
 myModMask = mod4Mask
-myDefaultLayout = Full
+myDefaultLayout = avoidStruts layout
+  where layout = Full ||| tiled ||| Mirror tiled
+        tiled = Tall nmaster ratioDelta ratio
+        nmaster = 1
+        ratioDelta = 3/100
+        ratio = 1/2
 -- myKeys :: XConfig Layout -> DMI.Map (XMonad.ButtomMask, KeySym) (X ())
 myKeys = undefined
-
-
-data DefTest = DefText { string :: String
-                       , int    :: Integer
-                       }
-               deriving Show
-
-instance Default DefTest where
-  def = DefText { string = "foo"
-                , int    = 0
-                }
