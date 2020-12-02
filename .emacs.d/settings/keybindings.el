f6f044afb6b911cbf9376f510f8d4e75abb83b73
Vladislav Zhavoronkov <v.zhvrnkv@gmail.com>
2020-12-02 23:24:58 +0600

    [arch] on emacs add paredit and some update to sane-defaults

--- .emacs.d/settings/keybindings.el
+++ .emacs.d/settings/keybindings.el
@@ -22,6 +22,6 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
-(global-set-key (kbd "C-c j") 'ace-jump-mode)
+(global-set-key (kbd "C-j") 'ace-jump-mode)
 
 (provide 'keybindings)
