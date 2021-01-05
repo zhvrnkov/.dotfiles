0c648403e075886c617326f54fc558812137e293
Vladislav Zhavoronkov <v.zhvrnkv@gmail.com>
2021-01-06 01:04:47 +0600

    [arch] in emacs add package-management system
    as usual, stole it from Magnars repo -- list installed packages and
    install them if not installed

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,3 @@
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
@@ -13,6 +10,42 @@
       (expand-file-name "settings" user-emacs-directory))
 
 (add-to-list 'load-path settings-dir)
+
+;; Setup packages
+(require 'setup-package)
+
+;; Install extensions if they're missing
+(defun init--install-packages ()
+  (packages-install
+   '(ace-jump-mode
+     browse-kill-ring
+     doom-themes
+     exec-path-from-shell
+     gruber-darker-theme
+     haskell-mode
+     ido-completing-read+
+     ido-vertical-mode
+     json-mode
+     magit
+     markdown-mode
+     multiple-cursors
+     nasm-mode
+     paredit
+     plantuml-mode
+     rust-mode
+     smex
+     swift-mode
+     undo-tree
+     visual-regexp-steroids
+     visual-regexp
+     vterm
+     yaml-mode)))
+
+(condition-case nil
+    (init--install-packages)
+  (error
+   (package-refresh-contents)
+   (init--install-packages)))
 
 ;; PATH
 (require 'setup-path)
